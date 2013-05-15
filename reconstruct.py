ae8fda5d465c08bdf22899418d7585265da308d6
sb <sbenthall@gmail.com>
2013-05-14 22:51:57 -0700

    backwards algorithm

--- reconstruct.py
+++ reconstruct.py
@@ -58,14 +58,103 @@ def reconstruct_lineage_forward(reposts):
 
     return lineage
 
+# Whereas the above algorithm attempts to allocate
+# probability moving forward through the posts in time,
+# this algorithm starts from the latest post,
+# reallocating probability when it encounters
+# recorded reposts.
+#
+def reconstruct_lineage_backward(reposts):
+    n = reposts.shape[0] # number of reposts
+    recorded_reposts = np.sum(reposts[:,2]);
+
+    lineage = zeros((n, n))
+
+    #number of followers of user reposting
+    followers = reposts[:,1]
+    
+    #variables to increment
+    #number of reposts unaccounted for by reposts_count
+    #don't count the first one
+    unrecorded = n - recorded_reposts - 1
+
+    #recorded subsequent repost weight for each post
+    weights = np.copy(reposts[:,2])
+
+    for post_i in range(n-1,1,-1):
+
+        #if unrecorded, likelihood of having a particular parent
+        parent_likelihood_unrecorded = followers[0:post_i] / np.sum(followers[0:post_i])
+        plu_padded = np.hstack((np.atleast_2d(parent_likelihood_unrecorded), zeros((1, n - post_i))))
+        lineage[post_i,:] = plu_padded
+
+        if weights[post_i] > 0:
+            p_reposted_from_this = weights[post_i] / (n - post_i)
+            lineage[post_i+1:n,post_i+1:n] = lineage[post_i+1:n,post_i+1:n] * (1 - p_reposted_from_this)
+            lineage[post_i+1:n,post_i] = np.ones((n-post_i-1,1)).squeeze() * p_reposted_from_this
+
+        if not (np.sum(lineage[post_i,:]) > .99999 and np.sum(lineage[post_i,:]) < 1.00001):
+            print(np.sum(lineage[post_i,:]))
+            import pdb; pdb.set_trace()
+
+    return lineage
+
+
+def lineage_depths(lineage):
+    n = lineage.shape[0]
+    
+    depths = zeros((n,1))
+
+    for i in range(1,n):
+        depths[i] = 1 + np.dot(lineage[i,:], depths)
+
+    return depths
+
+def lineage_descendents(lineage):
+    n = lineage.shape[0]
+
+    descendents = zeros((n,1))
+
+    for k in range(n-1,0,-1):
+        desc = np.dot(lineage[:,k], 1 + descendents)
+
+        descendents[k] = desc
+
+    return descendents
+
+lineages = list()
+
 for k,d in data.items():
-    lineage = reconstruct_lineage_forward(d)
+    lineage = reconstruct_lineage_backward(d)
     lineages.append(lineage)
     #print(lineage)
     print(k)
+
+    fig = plt.figure()
     plt.imshow(lineage, cmap=cm.spectral)
     plt.title("Probabilistic lineage of reposts for feed %d" % (k))
     plt.xlabel("Parent post")
     plt.ylabel("Posts in chronological order")
+    #plt.show()
+
+    depths = lineage_depths(lineage)
+    descendents = lineage_descendents(lineage)
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(depths,'bo')
+    ax1.set_xlabel('Chronologically ordered posts')
+    ax1.set_ylabel('Expected depths',color='b')
+    for tl in ax1.get_yticklabels():
+        tl.set_color('b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(descendents,'r.')
+    ax2.plot(d[:,2],'g.')
+    ax2.set_ylabel('Expected total descendents',color='r')
+    ax2.set_yscale('log')
+    for tl in ax2.get_yticklabels():
+        tl.set_color('r')
+
     plt.show()
+
 
